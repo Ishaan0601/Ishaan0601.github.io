551e79c1c198e41b25745dd17a773dea1ded0582
Ishaan Virani <77121609+Ishaan0601@users.noreply.github.com>
2023-06-10 13:40:29 -0500

    website v2

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Hi, my name is Ishaan Virani and I am a motivated and dedicated student of computer science at The University of Texas at Dallas. My educational pursuits are centered on gaining a deep understanding of the theories and practices of computer science, with a focus on software development.
+
+As an aspiring software developer, I possess a wealth of knowledge and practical experience in a variety of programming languages, including Java, Javascript, C++, Python, HTML/CSS, and Swift. My technical proficiency is supplemented by a strong work ethic and attention to detail, allowing me to deliver high-quality results consistently.
+
+Driven by my passion for technology and a desire to contribute to the field of software development, I am committed to furthering my knowledge and skills in this domain. I am eager to collaborate with like-minded individuals and organizations to create innovative solutions that address real-world problems.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,201 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  UTDesign,
+  redCross,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer Intern",
+    company_name: "UTDesign",
+    icon: UTDesign,
+    //iconBg: "#383E56",
+    iconBg: "#E6DEDD",
+    date: "January 2023 - May 2023",
+    points: [
+      "Collaborated with a team of 5 members on the Tool Crib initiative to create an inventory and team database consisting of 300+ tools and 50+ teams.",
+      "Utilized React.js, MySQL, and Node.js to create a web application which checked-in and checked-out tools and notified admins of late returns as well as emailed students who have not returned their items.",
+      "Developed a dynamic web application utilizing the MERN stack resulting in a highly responsive and user-friendly application.",
+    ],
+  },
+  {
+    title: "Youth Placement Coordinator, Youth Placement Specialist",
+    company_name: "The American Red Cross",
+    icon: redCross,
+    iconBg: "#E6DEDD",
+    date: "October 2020 - May 2022",
+    points: [
+      "Organized and conducted interviews for a pool of prospective youth volunteers, adeptly assessing candidates' suitability.",
+      "Worked with over 30 Red Cross Clubs to merge 500+ student volunteers into the Red Cross database.",
+      "Coordinated a team of 8 Youth Placement Specialists and increased prospective volunteer intake by 40%.",
+    ],
+  },
+
+];
+
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, projects };
